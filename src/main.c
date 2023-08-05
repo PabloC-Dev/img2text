e22b2873cfb9a96fb78f564c15753d6912ec43e0
Pablo <pacema1@gmail.com>
2023-08-04 21:44:00 -0400

    act. 4/8/2023 21:43

--- src/main.c
+++ src/main.c
@@ -19,6 +19,7 @@ int main(int argc, char * argv[])
 	
 	char nombre_img[100];
 	char nombre_img_ftm[115];
+	char nombre_arreglo[115];
 	if (argv[1]==NULL){
 		printf("debe ingresar el nombre del archivo con la extenci\242n, ej. imagen.jpg");
 		getch();
@@ -35,7 +36,35 @@ int main(int argc, char * argv[])
 	lee_img = fopen(nombre_img,"rb");
 	crea_txt = fopen(nombre_img_ftm,"w");
 
-	
+	int valor,contador=0;
+	char hex[11];
+
+	strcpy(nombre_arreglo,"int ");
+	strcat(nombre_arreglo,nombre_img);
+	strcat(nombre_arreglo,"[]={\n");
+	fputs(nombre_arreglo,crea_txt);
+	while(!feof(lee_img)){
+		//fread(&valor,sizeof(int),1,archivo);
+		fread(&valor,sizeof(int),1,lee_img);
+		//fputc('"',crea_txt);
+		// 1° forma
+		//fwrite(&valor,sizeof(int),1,crea_txt);
+		
+		// 2° forma
+		//sprintf(hex,"%X",valor);
+		//fwrite(hex,sizeof(int),1,crea_txt);
+		
+		// 3° forma
+		fprintf(crea_txt,"%X",valor);
+		
+		//fputc('"',crea_txt);
+		
+		contador++;
+		if(!feof(lee_img)) fputc(',',crea_txt);
+		if((contador % 16) == 0) fputc('\n',crea_txt);
+		
+	}
+	fputs("};",crea_txt);
 	puts(nombre_img_ftm);
 	fclose(lee_img);
 	fclose(crea_txt);
